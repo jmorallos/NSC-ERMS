b1fdee5156e2e53070a2f654be79a89b7e87057c
jmorallos <joevermorallos2001@gmail.com>
2026-05-04 21:26:03 +0800

    add: table but need fixing

---
+++ src/renderer/assets/components/table.js
@@ -0,0 +1,21 @@
+export const table =
+/*html*/`
+<table>
+    <thead>
+        <tr>
+            <th>
+
+            </th>
+            <th></th>
+            <th>
+            </th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+`;
